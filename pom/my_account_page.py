dc59676ee1023a6f0f504793605686e9e49f6634
Ndubueze <ndubueze.egbulefu@gmail.com>
2022-02-15 15:54:27 +0100

    Added random code for email generation

--- pom/my_account_page.py
+++ pom/my_account_page.py
@@ -1,3 +1,7 @@
+import random
+import string
+
+
 class MyAccount:
     Order_History = "text=Order history and details"
     Credit_Slips = "text=My credit slips"
@@ -5,9 +9,14 @@ class MyAccount:
 
 
 class NewUser:
-    email_field= "input[name=\"email_create\"]"
-    first_name_field= "input[name=\"customer_firstname\"]"
-    last_name_field= "input[name=\"customer_lastname\"]"
-    user_email = "hunter1000@yahoo.com"
+    def random_char(y):
+        return "".join(random.choice(string.ascii_lowercase) for x in range(y))
+    email_field = "input[name=\"email_create\"]"
+    first_name_field = "input[name=\"customer_firstname\"]"
+    last_name_field = "input[name=\"customer_lastname\"]"
+    user_email = random_char(8)+"@yahoo.com"
     user_firstname = "Jane"
     user_last_name = "Doe"
+
+
+
